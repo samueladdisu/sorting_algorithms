d572a91e2f5cd02e5bbfd4e8e9c92751c17e39f5
BezaB10 <beza_b@outlook.comq qgit config--qgit config -- qgit config --global user.email beza_b@outlook.com>
2022-07-16 11:04:10 -0700

    select

---
+++ 2-selection_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array in ascending order
+ * @array: array of int
+ * @size: size of the array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+        size_t i, j, min;
+        int tmp;
+
+        if (array == NULL || size < 2)
+                return;
+
+        for (i = 0; i < size - 1; i++)
+        {
+		min = i;
+                for (j = i + 1; j < size; j++)
+                {
+                        if (array[j] < array[min])
+                        {
+                                min = j;
+                        }
+		}
+		if (i != min)
+		{
+			tmp = array[i];
+			array[i] = array[min];
+			array[min] = tmp;
+			print_array(array, size);
+		}
+	}
+}
+
--- sort.h
+++ sort.h
@@ -5,5 +5,6 @@
 
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
+void selection_sort(int *array, size_t size);
 
 #endif
