163b4a81f6d9e65a831c5be26d6bbb85f37f029a
samueladdisu <samueladdisu7@gmail.com>
2022-07-17 09:55:57 +0300

    quick sort

--- sort.h
+++ sort.h
@@ -6,5 +6,9 @@
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+int quick_sort2(int *array, size_t left, size_t right);
+int partition(int *array, int pivot, size_t left, size_t right);
+void swap(int *array, size_t firstIndex, size_t secondIndex);
 
 #endif
