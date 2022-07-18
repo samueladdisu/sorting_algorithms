2c2b29a5c8dd769e0d3ef0c62b5e6285c674dbb7
samueladdisu <samueladdisu7@gmail.com>
2022-07-18 20:44:25 +0300

    shell sort

--- sort.h
+++ sort.h
@@ -7,5 +7,6 @@ void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif
